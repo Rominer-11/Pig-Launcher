137ee66e587a6a82ab2372ea9c28e29bb4e4cd6b
Rominer-11 <75665420+Rominer-11@users.noreply.github.com>
2022-08-07 14:57:14 -0700

    Initial commit

---
+++ src/main/java/me/rominer_11/piglauncher/Piglauncher.java
@@ -0,0 +1,42 @@
+package me.rominer_11.piglauncher;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Pig;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public final class Piglauncher extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+
+        getCommand("play").setExecutor(new Play());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getPlayer().getItemInHand().getItemMeta().getDisplayName().equals("e")) {
+            TNTPrimed pig = event.getPlayer().getWorld().spawn(event.getPlayer().getEyeLocation(), TNTPrimed.class);
+            pig.setVelocity(new Vector(event.getPlayer().getLocation().getDirection().getX() * 3, event.getPlayer().getLocation().getDirection().getY() * 3, event.getPlayer().getLocation().getDirection().getZ() * 3));
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        event.getEntity().setGameMode(GameMode.SPECTATOR);
+    }
+
+}
---
+++ src/main/java/me/rominer_11/piglauncher/Play.java
@@ -0,0 +1,75 @@
+package me.rominer_11.piglauncher;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Play implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+
+            int border = Integer.parseInt(args[0]);
+
+            ItemStack gun = new ItemStack(Material.DIAMOND_HOE, 1);
+            ItemMeta gunMeta = gun.getItemMeta();
+            gunMeta.setDisplayName("e");
+            gunMeta.addEnchant(Enchantment.DAMAGE_ALL, 4, true);
+            gun.setItemMeta(gunMeta);
+
+            ItemStack glass = new ItemStack(Material.GLASS, 64);
+
+            ItemStack pearls = new ItemStack(Material.ENDER_PEARL, 64);
+
+            ItemStack apples = new ItemStack(Material.GOLDEN_APPLE, 64);
+
+            ItemStack helm = new ItemStack(Material.DIAMOND_HELMET, 1);
+            ItemMeta helmMeta = helm.getItemMeta();
+            helmMeta.addEnchant(Enchantment.PROTECTION_EXPLOSIONS, 2, true);
+            helm.setItemMeta(helmMeta);
+            ItemStack chest = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+            ItemMeta chestMeta = chest.getItemMeta();
+            chestMeta.addEnchant(Enchantment.PROTECTION_EXPLOSIONS, 2, true);
+            chest.setItemMeta(chestMeta);
+            ItemStack pants = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+            ItemMeta pantsMeta = pants.getItemMeta();
+            pantsMeta.addEnchant(Enchantment.PROTECTION_EXPLOSIONS, 2, true);
+            pants.setItemMeta(pantsMeta);
+            ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
+            ItemMeta bootsMeta = boots.getItemMeta();
+            bootsMeta.addEnchant(Enchantment.PROTECTION_EXPLOSIONS, 2, true);
+            boots.setItemMeta(bootsMeta);
+
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.getInventory().addItem(gun);
+                player.getInventory().addItem(glass);
+                player.getInventory().addItem(pearls);
+                player.getInventory().addItem(apples);
+                player.getInventory().addItem(helm);
+                player.getInventory().addItem(chest);
+                player.getInventory().addItem(pants);
+                player.getInventory().addItem(boots);
+
+                player.setGameMode(GameMode.SURVIVAL);
+
+                ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+                Bukkit.getServer().dispatchCommand(console, "spreadplayers 0 0 " + (border - 25) + "" + (border - 25) + " true @a");
+                System.out.println("spreadplayers 0 0 " + (border - 25) + " " + (border - 25) + " true @a");
+                Bukkit.getServer().dispatchCommand(console, "worldborder set " + border);
+            }
+
+        }
+
+        return false;
+    }
+}
